92a0f8bfe401470bb281e07516164988fde5ab85
kuprelis <simas.kuprelis@gmail.com>
2017-04-21 00:16:56 +0300

    refactored code, implemented node parsing, class data type fixes

--- main.py
+++ main.py
@@ -4,6 +4,29 @@ import requests
 import json
 
 
+class Node:
+    def __init__(self, name, cat):
+        self.name = name
+        self.cat = cat  # teacher = 0, room = 1, student = 2
+        self.groups = set()
+
+
+class Group:
+    def __init__(self, name):
+        self.name = name
+        self.lessons = []
+        self.teachers = []
+        self.rooms = []
+        self.students = []
+
+
+# print("Įveskite tvarkaraščių tinklapio index.htm adresą:\n")
+# index_url = input()
+base_url = "http://www.azuolynas.klaipeda.lm.lt/tvark/tvark_2016-2017_2pusm/"
+groups = {}
+nodes = {}
+
+
 def decode_html(html_string):
     converted = UnicodeDammit(html_string)
     if not converted.unicode_markup:
@@ -12,62 +35,48 @@ def decode_html(html_string):
             ', '.join(converted.tried_encodings))
     return converted.unicode_markup
 
-# print("Įveskite tvarkaraščių tinklapio index.htm adresą:\n")
-# index_url = input()
-base_url = "http://www.azuolynas.klaipeda.lm.lt/tvark/tvark_2016-2017_2pusm/"
 
-page = requests.get(base_url + "index.htm")
-tree = html.fromstring(decode_html(page.content))
-tree.make_links_absolute(base_url)
-
-# ----------------------------------------------------------------------------------------------------------------------
+def get_tree(page_url):
+    page = requests.get(page_url)
+    tree = html.fromstring(decode_html(page.content))
+    tree.make_links_absolute(base_url)
+    return tree
 
 
-class Node:
-    def __init__(self, name, cat):
-        self.name = name
-        self.cat = cat  # category
-        self.groups = []
-
-    def add_group(self, group_id):
-        self.groups.append(group_id)
-
-
-class Group:
-    def __init__(self, week_day, lesson_num, teacher, room):
-        self.week_day = week_day
-        self.lesson_num = lesson_num
-        self.teacher = teacher
-        self.room = room
-        self.students = []
-
-    def add_student(self, student_id):
-        self.students.append(student_id)
-
-groups = {}
-nodes = {}
+def get_id(page_url):
+    for i in range(-5, -len(page_url), -1):
+        if page_url[i] == '/':
+            return page_url[i + 1:-4]
 
 
 def create_group(group_url):
     return
 
 
-def create_node(node_url, node_type):
+def create_node(node_url, node_cat):
+    node_tree = get_tree(node_url)
+    node_name = node_tree.xpath("/html/body/center[2]/table/tr[1]/td/font/b/text()")[0]
+    node = Node(node_name, node_cat)
+    links = node_tree.xpath("//a/@href")
+    for i in range(1, len(links) - 1):
+        create_group(links[i])
+        node.groups.add(get_id(links[i]))
+    nodes[get_id(node_url)] = node
     return
 
 
-row_count = int(tree.xpath("count(/html/body/center[2]/center/table//tr)"))
+index_tree = get_tree(base_url + "index.htm")
+row_count = int(index_tree.xpath("count(/html/body/center[2]/center/table//tr)"))
 current_cat = 0
+
 for i in range(2, row_count + 1):
-    if int(tree.xpath("count(" + "/html/body/center[2]/center/table/tr[{}]/td".format(i) + ")")) == 1:
+    if int(index_tree.xpath("count(" + "/html/body/center[2]/center/table/tr[{}]/td".format(i) + ")")) == 1:
         current_cat += 1
         continue
 
     for j in range(1, 4):
-        url = tree.xpath("/html/body/center[2]/center/table/tr[{}]/td[{}]/a/@href".format(i, j))
+        url = index_tree.xpath("/html/body/center[2]/center/table/tr[{}]/td[{}]/a/@href".format(i, j))
         if len(url) > 0:
             create_node(url[0], current_cat)
         else:
             break
-
-# json_data = json.dumps(data)
