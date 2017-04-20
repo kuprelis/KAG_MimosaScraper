6296acebf403fc23996e9dd22d05b77501c4b356
Simas Kuprelis <simas.kuprelis@gmail.com>
2017-04-20 15:34:07 +0300

    added classes, fixed variable names

--- main.py
+++ main.py
@@ -20,25 +20,53 @@ page = requests.get(base_url + "index.htm")
 tree = html.fromstring(decode_html(page.content))
 tree.make_links_absolute(base_url)
 
-nodes = []
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+class Node:
+    def __init__(self, name, cat):
+        self.name = name
+        self.cat = cat  # category
+        self.groups = []
+
+    def add_group(self, group_id):
+        self.groups.append(group_id)
+
+
+class Group:
+    def __init__(self, week_day, lesson_num, teacher, room):
+        self.week_day = week_day
+        self.lesson_num = lesson_num
+        self.teacher = teacher
+        self.room = room
+        self.students = []
+
+    def add_student(self, student_id):
+        self.students.append(student_id)
+
 groups = {}
+nodes = {}
 
 
-def create_node(link, type):
-    print(link, type)
+def create_group(group_url):
+    return
+
+
+def create_node(node_url, node_type):
+    return
 
 
 row_count = int(tree.xpath("count(/html/body/center[2]/center/table//tr)"))
-node_type = 0
+current_cat = 0
 for i in range(2, row_count + 1):
     if int(tree.xpath("count(" + "/html/body/center[2]/center/table/tr[{}]/td".format(i) + ")")) == 1:
-        node_type += 1
+        current_cat += 1
         continue
 
     for j in range(1, 4):
-        link = tree.xpath("/html/body/center[2]/center/table/tr[{}]/td[{}]/a/@href".format(i, j))
-        if len(link) > 0:
-            create_node(link[0], node_type)
+        url = tree.xpath("/html/body/center[2]/center/table/tr[{}]/td[{}]/a/@href".format(i, j))
+        if len(url) > 0:
+            create_node(url[0], current_cat)
         else:
             break
 
