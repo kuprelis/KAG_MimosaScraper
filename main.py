c8320ade178097e5879165276badb7cfa3771b4e
Simas Kuprelis <simas.kuprelis@gmail.com>
2017-04-25 00:13:56 +0300

    removed absolute links, added group parsing (WIP), simplified code

--- main.py
+++ main.py
@@ -20,9 +20,10 @@ class Group:
         self.students = []
 
 
-# print("Įveskite tvarkaraščių tinklapio index.htm adresą:\n")
-# index_url = input()
+# print("Įveskite tvarkaraščių tinklapio index.htm adresą (pvz. http://www.example.com/*/index.htm)\n")
+# base_url = input()[:-9]
 base_url = "http://www.azuolynas.klaipeda.lm.lt/tvark/tvark_2016-2017_2pusm/"
+index_url = "index.htm"
 groups = {}
 nodes = {}
 
@@ -37,46 +38,54 @@ def decode_html(html_string):
 
 
 def get_tree(page_url):
-    page = requests.get(page_url)
+    page = requests.get(base_url + page_url)
     tree = html.fromstring(decode_html(page.content))
-    tree.make_links_absolute(base_url)
     return tree
 
 
 def get_id(page_url):
-    for i in range(-5, -len(page_url), -1):
-        if page_url[i] == '/':
-            return page_url[i + 1:-4]
+    return page_url[:-4]
 
 
 def create_group(group_url):
+    """
+    group_tree = get_tree(group_url)
+    group_table = group_tree
+    top_string = group_tree.xpath("/html/body/center[2]/table/tr[1]/td/text()")[0]
+
+    a, b = None, None
+    for i in range(len(top_string)):
+        if top_string[i] == '\x97':
+            if a is None:
+                a = i + 2
+            else:
+                b = i - 1
+                break
+    group_name = top_string[a:b]
+
+    print(group_name)
+    """
     return
 
 
 def create_node(node_url, node_cat):
-    node_tree = get_tree(node_url)
-    node_name = node_tree.xpath("/html/body/center[2]/table/tr[1]/td/font/b/text()")[0]
+    node_table = get_tree(node_url).xpath("/html/body/center[2]/table")[0]
+    node_name = node_table.xpath("./tr[1]/td/font/b/text()")[0]
     node = Node(node_name, node_cat)
-    links = node_tree.xpath("//a/@href")
-    for i in range(1, len(links) - 1):
-        create_group(links[i])
-        node.groups.add(get_id(links[i]))
+    for group in node_table.xpath(".//a/@href"):
+        if group == index_url:
+            continue
+        create_group(group)
+        node.groups.add(get_id(group))
     nodes[get_id(node_url)] = node
     return
 
-
-index_tree = get_tree(base_url + "index.htm")
-row_count = int(index_tree.xpath("count(/html/body/center[2]/center/table//tr)"))
 current_cat = 0
-
-for i in range(2, row_count + 1):
-    if int(index_tree.xpath("count(" + "/html/body/center[2]/center/table/tr[{}]/td".format(i) + ")")) == 1:
+# create_group("3_it_p_2_1.htm")
+for row in get_tree(index_url).xpath("/html/body/center[2]/center/table/tr"):
+    if int(row.xpath("count(./td)")) == 1:
         current_cat += 1
         continue
 
-    for j in range(1, 4):
-        url = index_tree.xpath("/html/body/center[2]/center/table/tr[{}]/td[{}]/a/@href".format(i, j))
-        if len(url) > 0:
-            create_node(url[0], current_cat)
-        else:
-            break
+    for link in row.xpath(".//a/@href"):
+        create_node(link, current_cat)
