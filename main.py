01b74981d98f0aab072e5cb1aef514e324b0908d
Simas Kuprelis <simas.kuprelis@gmail.com>
2017-04-25 19:52:51 +0300

    added encoder template, fixed get_id for groups, fully implemented time parsing, simplified code, added file output, delegated group name parsing to create_node

--- main.py
+++ main.py
@@ -15,7 +15,16 @@ class Group:
     def __init__(self, name):
         self.name = name
         self.times = []
-        self.nodes = []
+        self.nodes = []  # (day, lesson number)
+
+
+class CustomEncoder(json.JSONEncoder):  # TODO fix this
+    def default(self, obj):
+        if isinstance(obj, Node):
+            return obj.name
+        elif isinstance(obj, Group):
+            return obj.name
+        return json.JSONEncoder.default(self, obj)
 
 
 # print("Įveskite tvarkaraščių tinklapio index.htm adresą (pvz. http://www.example.com/*/index.htm)\n")
@@ -41,36 +50,30 @@ def get_tree(page_url):
     return tree
 
 
-def get_id(page_url):
+def get_id(page_url, is_node):
+    if is_node:
+        return page_url[5:-4]
     return page_url[:-4]
 
 
-def create_group(group_url):
-    if get_id(group_url) in groups:
+def create_group(group_url, group_name):
+    if get_id(group_url, False) in groups:
         return
 
-    group_table = get_tree(group_url).xpath("/html/body/center[2]/table")[0]
-    top_string = group_table.xpath("./tr[1]/td/text()")[0]
+    table = get_tree(group_url).xpath("/html/body/center[2]/table")[0]
+    group = Group(group_name)
 
-    a, b = None, None
-    for i in range(len(top_string)):
-        if top_string[i] == '\x97':
-            if a is None:
-                a = i + 2
-            else:
-                b = i - 1
-                break
-    group = Group(top_string[a:b])
+    for node in table.xpath("./tr[2]/td[1]/text()"):
+        if len(node) <= 2:
+            continue
+        node_id = node[:node.index(" ")].lstrip()  # trim invisible chars
+        group.nodes.append(str.lower(node_id))
 
-    for node in group_table.xpath("./tr[2]/td[1]/text()"):
-        for i in range(len(node)):
-            if node[i] == '\x97':
-                group.nodes.append(node[2:i - 1])  # ignore 2 invisible chars
-                break
-
-    for time in group_table.xpath("./tr[2]/td[2]/text()"):
+    # TODO get room node from time
+    for time in table.xpath("./tr[2]/td[2]/text()"):
         time = time[2:]
         day = time[:3]
+
         if day == "Pir":
             day = 1
         elif day == "Ant":
@@ -84,25 +87,36 @@ def create_group(group_url):
         else:
             continue
 
-    groups[get_id(group_url)] = group
+        if time[5] == "-":
+            for i in range(int(time[4]), int(time[6]) + 1):
+                group.times.append((day, i))
+        else:
+            group.times.append((day, int(time[4])))
+
+    groups[get_id(group_url, False)] = group
     return
 
 
 def create_node(node_url, node_cat):
-    node_table = get_tree(node_url).xpath("/html/body/center[2]/table")[0]
-    node_name = node_table.xpath("./tr[1]/td/font/b/text()")[0]
-    node = Node(node_name, node_cat)
-    for group in node_table.xpath(".//a/@href"):
-        if group == index_url:
+    # TODO check result code
+    table = get_tree(node_url).xpath("/html/body/center[2]/table")[0]
+    name = table.xpath("./tr[1]/td/font/b/text()")[0]
+    node = Node(name.rstrip(), node_cat)  # trim spaces on the right
+
+    for group in table.xpath(".//a"):
+        url = group.xpath("./@href")[0]
+        if url == index_url:
             continue
-        create_group(group)
-        node.groups.add(get_id(group))
-    nodes[get_id(node_url)] = node
+        name = group.xpath("./text()")[0]
+        create_group(url, name)
+        node.groups.add(get_id(url, False))
+
+    nodes[get_id(node_url, True)] = node
     return
 
 current_cat = 0
-create_group("3_kkb_2_2.htm")
-"""
+# create_group("3_lie_jov_1_45.htm", "tst")
+
 for row in get_tree(index_url).xpath("/html/body/center[2]/center/table/tr"):
     if int(row.xpath("count(./td)")) == 1:
         current_cat += 1
@@ -110,4 +124,9 @@ for row in get_tree(index_url).xpath("/html/body/center[2]/center/table/tr"):
 
     for link in row.xpath(".//a/@href"):
         create_node(link, current_cat)
-"""
+
+with open("groups.json", "w") as output:
+    json.dump(groups, output, cls=CustomEncoder)
+
+with open("nodes.json", "w") as output:
+    json.dump(nodes, output, cls=CustomEncoder)
